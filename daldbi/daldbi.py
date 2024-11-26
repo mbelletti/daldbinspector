3e4845f328a9387e1c470176d371f8c84d1dbb02
Massimiliano Belletti <max@belletti.net>
2024-11-26 11:32:24 +0100

    fix def for view

--- daldbi/daldbi.py
+++ daldbi/daldbi.py
@@ -5,8 +5,10 @@ from pydal import adapters
 
 migrate = False
 
+
 class DbiAdapter:
     datatypes = {}
+
     def __init__(self, db, schema='public') -> None:
         super().__init__()
         self.db = db
@@ -34,7 +36,10 @@ class DbiSqlite(DbiAdapter):
     def get_fields(self, table) -> list:
         sql = "PRAGMA table_info('%s')" % table
         fields = self.db.executesql(sql, as_dict=True)
-        return [(f['name'], 'id' if f['pk'] == 1 else self.datatypes[f['type'].upper()]) for f in fields]
+        return [
+            (f['name'], 'id' if f['pk'] == 1 else self.datatypes[f['type'].upper()])
+            for f in fields
+        ]
 
     def get_tables(self) -> list:
         sql = """SELECT name as table_name FROM sqlite_master
@@ -43,6 +48,7 @@ class DbiSqlite(DbiAdapter):
 
         rows = self.db.executesql(sql, as_dict=True)
         return sorted([r['table_name'] for r in rows])
+
 
 class DbiPostgresql(DbiAdapter):
     adapter = adapters.postgres.Postgre
@@ -81,8 +87,9 @@ class DbiPostgresql(DbiAdapter):
         'character varying': 'string',
         'timestamp without time zone': 'datetime',
         'time without time zone': 'string',
-        'double precision': 'double'
+        'double precision': 'double',
     }
+
     def __init__(self, db, schema='public') -> None:
         super().__init__(db, schema=schema)
         self.schema = schema or 'public'
@@ -98,17 +105,24 @@ class DbiPostgresql(DbiAdapter):
             column_default
             FROM information_schema.columns
             WHERE table_schema = '%s' AND table_name='%s'
-            ORDER BY ordinal_position""" % (self.schema, table)
+            ORDER BY ordinal_position""" % (
+            self.schema,
+            table,
+        )
 
         fields = self.db.executesql(sql, as_dict=True)
         res = []
         for f in fields:
-            if (('column_default' in f) and
-                f['column_default'] and
-                f['column_default'].startswith('nextval')):
+            if (
+                ('column_default' in f)
+                and f['column_default']
+                and f['column_default'].startswith('nextval')
+            ):
                 res += [(f['column_name'], 'id')]
             elif f['data_type'] == 'numeric':
-                res += [(f['column_name'], 'decimal(%(numeric_precision)d, %(numeric_scale)d)' % f)]
+                _np = f.get('numeric_precision', 10)
+                _ns = f.get('numeric_scale', 2)
+                res += [(f['column_name'], f'decimal({_np}, {_ns}')]
             elif f['data_type'] != 'USER-DEFINED':
                 res += [(f['column_name'], self.datatypes[f['data_type']])]
         return res
@@ -120,14 +134,17 @@ class DbiPostgresql(DbiAdapter):
         rows = self.db.executesql(sql % self.schema, as_dict=True)
         return sorted([row['table_name'] for row in rows])
 
+
 class DbiAdapters:
     dbi_adapters = [DbiSqlite, DbiPostgresql]
+
     @classmethod
     def get_adapter(cls, db, schema):
         for adpt in cls.dbi_adapters:
             if isinstance(db._adapter, adpt.adapter):
                 return adpt(db, schema=schema)
         raise Exception('Adapter %s not found'.format(db._adapter.__class__))
+
 
 class DbInspector(object):
     def __init__(self, db, schema=None, auto_define=False):
@@ -146,26 +163,30 @@ class DbInspector(object):
         tpl = """db.define_table('%(tablename)s',%(sep)s%(fields)s%(sep)s%(sep)srname='%(rname)s',migrate=migrate)"""
 
         cond = lambda x: True if with_id else x != 'id'
-        flt_flds = ((f, t) for f, t in self.adapter.get_fields(table) if cond(f) )
+        flt_flds = ((f, t) for f, t in self.adapter.get_fields(table) if cond(f))
 
         params = {}
         params['sep'] = '\n\t\t'
 
-        fields = params['sep'].join(["Field('%s', type='%s')," %(f, t) for f, t in flt_flds ])
+        fields = params['sep'].join(
+            ["Field('%s', type='%s')," % (f, t) for f, t in flt_flds]
+        )
         if self.schema and self.schema != 'public':
-            params.update({'tablename': "%s_%s" % (self.schema, table), 'fields': fields})
+            params.update(
+                {'tablename': "%s_%s" % (self.schema, table), 'fields': fields}
+            )
         else:
             params.update({'tablename': table, 'fields': fields})
 
         if self.schema:
-            params['rname']= "%s.%s" % (self.schema, table)
+            params['rname'] = "%s.%s" % (self.schema, table)
         else:
             params['rname'] = table
         return tpl % params
 
     def _define_table(self, table):
         db = self.db
-        #if table.find('.') > -1:
+        # if table.find('.') > -1:
         #    self.schema, table = table.split('.')
         code = self.table_def(table)
         exec(code)
